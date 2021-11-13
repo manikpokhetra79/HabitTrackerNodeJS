70931c540c029762afac52380d1f8407237ffebe
Manik Gupta <manikpokhetra@gmail.com>
2021-11-13 21:26:56 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,19 +2,12 @@ const mongoose = require('mongoose');
 
 const uri = process.env.HABIT_URI;
 
-mongoose
-  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to Atlas database ');
-  })
-  .catch((err) => {
-    console.error(`Error connecting to the database. \n${err}`);
-  });
-//local db settingss
-// mongoose.connect('mongodb://localhost/habit_tracker');
-// const db = mongoose.connection;
 
-// db.on('error', console.error.bind(console, 'DB Error'));
-// db.once('open', () => {
-//   console.log('Connected to MongoDB');
-// });
+local db settingss
+mongoose.connect('mongodb://localhost/habit_tracker');
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'DB Error'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
